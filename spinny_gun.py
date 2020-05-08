851dfe9185a584cc8cec06d3256e4d28c8b757e9
Caleb Werth <cwerth@crimson.ua.edu>
2020-05-07 23:10:31 -0500

    pep8 styling

--- spinny_gun.py
+++ spinny_gun.py
@@ -1,6 +1,7 @@
-import pygame
+"""This file currently contains all the Spinny Gun code"""
 import random
 from math import cos, sin, radians
+import pygame # pylint: disable=import-error
 
 # Screen width and height
 DISPLAY_WIDTH = 800
@@ -10,11 +11,11 @@ DISPLAY_HEIGHT = 800
 WHITE = (255, 255, 255)
 GOLD = (218, 165, 32)
 
+# Initializing
 pygame.init()
-
 SCREEN = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("Spinny Gun")
-clock = pygame.time.Clock()
+CLOCK = pygame.time.Clock()
 
 
 def rot_center(image, rect, angle):
@@ -24,27 +25,32 @@ def rot_center(image, rect, angle):
     return rot_image, rot_rect
 
 
-class SpinnyGun:
+class SpinnyGun(object):
     """The rotating gun that the player can fire"""
 
-    def __init__(self, screen, position):
-        self.screen = screen
+    def __init__(self, display, position):
+        self.display = display
         self.image = pygame.image.load("assets/gun.png")
         self.rotated_image = self.image
         self.rect = self.image.get_rect(center=position)
         self.angle = 0
-        self.turningLeft = True
+        self.turning_left = True
 
     def blit(self):
-        self.screen.blit(self.rotated_image, self.rect)
+        """Draws gun on display"""
+        self.display.blit(self.rotated_image, self.rect)
 
     def rotate(self):
+        """
+        Rotates self a set number of degrees,
+        changing direction when needed
+        """
         if self.angle >= 60:
-            self.turningLeft = False
+            self.turning_left = False
         elif self.angle <= -60:
-            self.turningLeft = True
+            self.turning_left = True
 
-        if self.turningLeft:
+        if self.turning_left:
             self.angle += 1
         else:
             self.angle -= 1
@@ -52,56 +58,60 @@ class SpinnyGun:
         self.rotated_image, self.rect = rot_center(self.image, self.rect, self.angle)
 
 
-class Projectile:
+class Projectile(object):
     """This is what the Spinny Gun fires"""
 
-    def __init__(self, screen, position, angle):
-        self.screen = screen
-        self.x = position[0]
-        self.y = position[1]
+    def __init__(self, display, position, angle):
+        self.display = display
+        self.x_pos = position[0]
+        self.y_pos = position[1]
         self.speed = 5
         self.x_vel = -round(self.speed * sin(radians(angle)))
         self.y_vel = -round(self.speed * cos(radians(angle)))
         self.radius = 8
 
     def draw(self):
-        pygame.draw.circle(self.screen, GOLD, (self.x, self.y), self.radius)
+        """Draws circle on display at x and y position"""
+        pygame.draw.circle(self.display, GOLD, (self.x_pos, self.y_pos), self.radius)
 
     def move(self):
-        self.x += self.x_vel
-        self.y += self.y_vel
+        """Moves according to x and y velocity"""
+        self.x_pos += self.x_vel
+        self.y_pos += self.y_vel
 
 
-class Missile:
+class Missile(object):
     """These missiles rain from the sky to attack the player"""
 
-    def __init__(self, screen):
-        self.screen = screen
+    def __init__(self, display):
+        self.display = display
         self.image = pygame.image.load("assets/missiles/missile-1_fly-0.png")
         self.rotated_image = pygame.transform.rotate(self.image, 180)
         self.speed = 5
-        self.x = random.randrange(0, DISPLAY_WIDTH)
-        self.y = -600
+        self.x_pos = random.randrange(0, DISPLAY_WIDTH)
+        self.y_pos = -600
 
     def blit(self):
-        self.screen.blit(self.rotated_image, (self.x, self.y))
+        """Draws self at current position"""
+        self.display.blit(self.rotated_image, (self.x_pos, self.y_pos))
 
     def move(self):
-        self.y += self.speed
+        """Updates y position to move down"""
+        self.y_pos += self.speed
 
 
 def game_loop():
     """The main game loop"""
 
-    gameExit = False
+    game_exit = False
     gun = SpinnyGun(SCREEN, (DISPLAY_WIDTH * 0.5, DISPLAY_HEIGHT * 0.875))
     missile = Missile(SCREEN)
     projectiles = []
 
-    while not gameExit:
+    while not game_exit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                game_exit = True
 
             # Fire a projectile if the player presses and releases space
             if event.type == pygame.KEYUP:
@@ -114,10 +124,10 @@ def game_loop():
         # If a projectile moves off-screen, remove it from the list
         for projectile in projectiles:
             if (
-                projectile.x > DISPLAY_WIDTH
-                or projectile.x < 0
-                or projectile.y > DISPLAY_HEIGHT
-                or projectile.y < 0
+                    projectile.x > DISPLAY_WIDTH
+                    or projectile.x < 0
+                    or projectile.y > DISPLAY_HEIGHT
+                    or projectile.y < 0
             ):
                 projectiles.pop(projectiles.index(projectile))
             projectile.move()
@@ -128,7 +138,7 @@ def game_loop():
         gun.blit()
 
         # If the missile gets to the bottom, replace it with a new missile
-        if missile.y > DISPLAY_HEIGHT:
+        if missile.y_pos > DISPLAY_HEIGHT:
             missile = Missile(SCREEN)
 
         # Move and draw missile
@@ -137,7 +147,7 @@ def game_loop():
 
         # Move all background changes to the foreground
         pygame.display.update()
-        clock.tick(60)
+        CLOCK.tick(60)
 
 
 game_loop()
