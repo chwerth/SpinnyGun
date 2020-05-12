b7a9fd85668bcdd898f569b352555882d106fdc8
Rw552 <rw552@hotmail.com>
2020-05-11 23:45:10 -0500

    formatting with black

--- setup.py
+++ setup.py
@@ -6,10 +6,14 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name = "Spinny Gun",
-    version = "1.0.0",
-    description = "test",
-    options = {"build_exe": {"packages": ["pygame"],
-                            "include_files": ['README.md','LICENSE','CREDITS.txt','assets']}},
-    executables = [Executable("spinny_gun.py", base=base)]
+    name="Spinny Gun",
+    version="1.0.0",
+    description="test",
+    options={
+        "build_exe": {
+            "packages": ["pygame"],
+            "include_files": ["README.md", "LICENSE", "CREDITS.txt", "assets"],
+        }
+    },
+    executables=[Executable("spinny_gun.py", base=base)],
 )
